bb1f2bfb0b15816e69e9f2a2f5cc7b07a5329f0f
IngelstenOlof <ingelstenolof@gmail.com>
2021-02-25 14:01:31 +0100

    Nothing works

--- OlofIngelsten/ToneJSExamples/index.js
+++ OlofIngelsten/ToneJSExamples/index.js
@@ -1,5 +1,7 @@
-import * as Tone from 'tone';
+import * as Tone from "./node_modules/tone/build/Tone.js";
 
+//create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
 
+//play a middle 'C' for the duration of an 8th note
 synth.triggerAttackRelease("C4", "8n");
