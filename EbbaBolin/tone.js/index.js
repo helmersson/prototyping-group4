2bf8837d8afdbb4a753fbb40cf9b310a4b47bd5d
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-25 19:34:29 +0100

    more event and style

--- EbbaBolin/tone.js/index.js
+++ EbbaBolin/tone.js/index.js
@@ -13,21 +13,29 @@ for(var i = 0; i < notes.length; i++){
     var note = notes[i];
     if (note == 'E' || note == 'B' )
     hasSharp = false;
-    html += `<div class = 'whitenote' onmousedown ='noteDown(this)'data-note='${note + (octave+4)}'>`;
     
+    html += `<div class='whitenote' onmousedown='noteDown(this, false)' onmouseup='noteUp(this,false)' onmouseleave='noteUp(this,false)' data-note='${note + (octave+4)}'>`;
+
     if (hasSharp) {
-    html += `<div class = 'blacknote' onmousedown ='noteDown(this)' data-note='${note + '#' + (octave+4)}'></div>`;
-        
+      html += `<div class='blacknote' onmousedown='noteDown(this, true)' onmouseup='noteUp(this, true)' onmouseleave='noteUp(this,true)' data-note='${note + '#' + (octave+4)}'>
+           
+      </div>`;
     }
+
     html += `</div>`; 
-}
+   }
 }
    
 document.getElementById("container").innerHTML = html;
 
-function noteDown (elem) {
+function noteUp (elem, isSharp){
+    elem.style.background = isSharp ? '#777' : 'white';
+}
+
+function noteDown (elem,isSharp) {
     var note = elem.dataset.note;
     //alert(note);
+    elem.style.background = isSharp ? 'black' : '#ccc';
     synth.triggerAttackRelease(note,"16n");
     event.stopPropagation();
 }
