abee7d386330dea28dfff6bc403c9d10de17fcb1
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-25 18:01:35 +0100

    div problem

--- EbbaBolin/tone.js/index.js
+++ EbbaBolin/tone.js/index.js
@@ -1,7 +1,22 @@
+var synth = new Tone.PolySynth().toMaster();
 var notes = ['C', 'D','E', 'F','G','A','B'];
 var html = "";
 for(var i = 0; i < notes.length; i++){
     var note = notes[i];
-    html += `<div class = "whitenote"></div>`;
+    html += `<div class = 'whitenote' onmousedown ='noteDown(this)' 
+    data-note='${note + '4'}'>`;
+    
+    html += `<div class = 'blacknote' onmousedown ='noteDown(this)' 
+        data-note='${note + '#4'}'></div>`;
+        
+    html += '</div';
 }
+
+   
 document.getElementById("container").innerHTML = html;
+
+function noteDown (elem) {
+    var note = elem.dataset.note;
+    //alert(note);
+    synth.triggerAttackRelease(note,"16n");
+}
