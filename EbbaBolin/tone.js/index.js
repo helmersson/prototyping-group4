63675dc5296793a46813d10e14dc7c1931a1f12a
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-25 19:00:54 +0100

    fixed

--- EbbaBolin/tone.js/index.js
+++ EbbaBolin/tone.js/index.js
@@ -1,15 +1,19 @@
+
 var synth = new Tone.PolySynth().toMaster();
 var notes = ['C', 'D','E', 'F','G','A','B'];
+
 var html = "";
 for(var i = 0; i < notes.length; i++){
+
     var note = notes[i];
-    html += `<div class = 'whitenote' onmousedown ='noteDown(this)' 
-    data-note='${note + '4'}'>`;
+
+    html += `<div class = 'whitenote' onmousedown ='noteDown(this)'data-note='${note + '4'}'>`;
     
-    html += `<div class = 'blacknote' onmousedown ='noteDown(this)' 
-        data-note='${note + '#4'}'></div>`;
+    
+    html += `<div class = 'blacknote' onmousedown ='noteDown(this)' data-note='${note + '#4'}'></div>`;
         
-    html += '</div';
+        
+    html += `</div>`; 
 }
 
    
