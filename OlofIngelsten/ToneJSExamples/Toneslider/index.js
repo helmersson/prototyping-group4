1f4e6ede71a69821bae245550b3274767a2dcaf3
IngelstenOlof <ingelstenolof@gmail.com>
2021-02-25 15:09:24 +0100

    Small updates

--- OlofIngelsten/ToneJSExamples/Toneslider/index.js
+++ OlofIngelsten/ToneJSExamples/Toneslider/index.js
@@ -1,11 +1,12 @@
 //create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.FMSynth().toDestination();
 
 //play a middle 'C' for the duration of an 8th note
 const slider = document.getElementById("slider");
 const offButton = document.getElementById("turnOff");
 const onButton = document.getElementById("turnOn");
 const herz = document.getElementById("herz");
+
 herz.innerHTML = slider.value;
 
 onButton.addEventListener("mousedown", e => {
