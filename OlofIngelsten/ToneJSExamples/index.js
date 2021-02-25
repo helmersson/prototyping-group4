13d5508858cda61a967fbe9757e04629fd2dfbf9
IngelstenOlof <ingelstenolof@gmail.com>
2021-02-25 14:52:52 +0100

    Finished example 1

--- OlofIngelsten/ToneJSExamples/index.js
+++ OlofIngelsten/ToneJSExamples/index.js
@@ -3,11 +3,19 @@ const synth = new Tone.Synth().toDestination();
 
 //play a middle 'C' for the duration of an 8th note
 const slider = document.getElementById("slider");
+const offButton = document.getElementById("turnOff");
+const onButton = document.getElementById("turnOn");
 const herz = document.getElementById("herz");
 herz.innerHTML = slider.value;
 
-slider.oninput = () => {
-  herz.innerHTML = slider.value;
-  //synth.triggerAttackRelease(slider.value);
 
-}
+onButton.addEventListener("mousedown", e => {
+  slider.oninput = () => {
+    herz.innerHTML = slider.value;
+    synth.triggerAttack(slider.value, "0", 0.2);
+  }
+});
+
+offButton.addEventListener("mousedown", e => {
+  synth.triggerRelease("1");
+})
