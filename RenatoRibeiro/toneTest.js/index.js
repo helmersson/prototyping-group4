28b20c2daf516a9cfbac6ad7615867f5e37ef77c
RenatoRibe <78792187+RenatoRibe@users.noreply.github.com>
2021-02-26 15:03:36 +0100

    create siren

--- RenatoRibeiro/toneTest.js/index.js
+++ RenatoRibeiro/toneTest.js/index.js
@@ -1,9 +1,36 @@
+
+const button = document.getElementById("button");
+
+//create a synth and connect it to the master output (your speakers)
 const synth = new Tone.MonoSynth({
     oscillator: {
         type: "square"
     },
     envelope: {
-        attack: 0.5
+        attack: 0.2
     }
 }).toDestination();
-synth.triggerAttackRelease("C4", "8n");
+
+
+function siren() {
+
+    now = Tone.now();
+    synth.triggerAttackRelease("C5", "8n", now);
+    synth.triggerAttackRelease("G4", "8n", now + 0.75);
+    synth.triggerAttackRelease("C5", "8n", now + 1.50);
+    synth.triggerAttackRelease("F#4", "8n", now + 2.25);
+    synth.triggerAttackRelease("B4", "8n", now + 3);
+    synth.triggerAttackRelease("F#4", "8n", now + 3.75);
+    synth.triggerAttackRelease("A#4", "8n", now + 4.50);
+    synth.triggerAttackRelease("F4", "8n", now + 5.25);
+
+
+}
+
+
+button.addEventListener("click", () => {
+    Tone.start();
+    siren();
+})
+
+
