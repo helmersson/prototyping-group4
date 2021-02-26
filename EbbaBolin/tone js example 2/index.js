6877af4740dc871146a3b0f0dcad5634a5343f56
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-26 14:37:27 +0100

    trying out new idea

--- EbbaBolin/tone js example 2/index.js
+++ EbbaBolin/tone js example 2/index.js
@@ -1,34 +1,60 @@
-const button = document.getElementById("button");
+//const button = document.getElementById("button");
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
 //const guitar = new Tone.Player("./sounds/guitar.wav").toDestination();
-const drums = new Tone.Player("./sounds/drums.wav").toDestination();
+//const drums = new Tone.Player("./sounds/drums.wav").toDestination();
 
-document.getElementById("button").addEventListener("mousedown", buttonGuitar);
-document.getElementById("button").addEventListener("mouseup", buttonGuitar2);
+document.getElementById("button").addEventListener("mousedown", buttonone);
+document.getElementById("button1").addEventListener("mousedown", buttontwo);
+document.getElementById("button2").addEventListener("mousedown", buttonthree);
+document.getElementById("button3").addEventListener("mousedown", buttonfour);
+document.getElementById("button4").addEventListener("mousedown", buttonfive);
+document.getElementById("button5").addEventListener("mousedown", buttonsix);
 
-  function buttonGuitar(){
+  function buttonone(){
   
     synth.triggerAttack("D4", now)
      document.getElementById("button").style.backgroundColor = "lightpink";
+     document.getElementById("button1").style.backgroundColor = "white";
     };
 
-    function buttonGuitar2(){
+    function buttontwo(){
   
       synth.triggerRelease(now+1)
+       document.getElementById("button1").style.backgroundColor = "lightpink";
        document.getElementById("button").style.backgroundColor = "white";
       };
-   
+      
+      function buttonthree(){
+  
+        synth.triggerAttack("F4", now)
+         document.getElementById("button2").style.backgroundColor = "lightpink";
+         document.getElementById("button3").style.backgroundColor = "white";
+        };
+    
+        function buttonfour(){
+      
+          synth.triggerRelease(now+1)
+           document.getElementById("button3").style.backgroundColor = "lightpink";
+           document.getElementById("button2").style.backgroundColor = "white";
+          };
+          
+      
+          function buttonfive(){
+  
+            synth.triggerAttack("A4", now)
+             document.getElementById("button4").style.backgroundColor = "lightpink";
+             document.getElementById("button5").style.backgroundColor = "white";
+            };
+        
+            function buttonsix(){
+          
+              synth.triggerRelease(now+1)
+               document.getElementById("button5").style.backgroundColor = "lightpink";
+               document.getElementById("button4").style.backgroundColor = "white";
+              };
 
   
 
-document.getElementById("button1").addEventListener("click", buttonDrums);
-
-function buttonDrums(){
-  Tone.loaded().then(() => {
-      drums.start();
-      document.getElementById("button1").style.backgroundColor = "lightpink";
-  });
-} 
 
