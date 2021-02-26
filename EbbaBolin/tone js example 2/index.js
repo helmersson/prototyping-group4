858f33ee31ce24b9a5158e2b8ce1c2f3ac1fc50a
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-26 10:16:48 +0100

    added one sound

--- EbbaBolin/tone js example 2/index.js
+++ EbbaBolin/tone js example 2/index.js
@@ -2,12 +2,11 @@ const button = document.getElementById("button");
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
-
+const guitar = new Tone.Player("./sounds/guitar.wav").toDestination();
 
 document.getElementById("button").addEventListener("click", mouseclick);
 
 function mouseclick (){
-    synth.triggerAttackRelease("C4", now)
 
   }
 
