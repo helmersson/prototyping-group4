cea1b5cce02669faa0faa8434980df4bdc127e36
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-26 14:01:26 +0100

    mouseevents

--- EbbaBolin/tone js example 2/index.js
+++ EbbaBolin/tone js example 2/index.js
@@ -2,22 +2,33 @@ const button = document.getElementById("button");
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
-const guitar = new Tone.Player("./sounds/guitar.wav").toDestination();
+//const guitar = new Tone.Player("./sounds/guitar.wav").toDestination();
 const drums = new Tone.Player("./sounds/drums.wav").toDestination();
 
-document.getElementById("button").addEventListener("click", buttonGuitar);
+document.getElementById("button").addEventListener("mousedown", buttonGuitar);
+document.getElementById("button").addEventListener("mouseup", buttonGuitar2);
 
   function buttonGuitar(){
-    Tone.loaded().then(() => {
-        guitar.start();
-    });
-  } 
+  
+    synth.triggerAttack("D4", now)
+     document.getElementById("button").style.backgroundColor = "lightpink";
+    };
 
+    function buttonGuitar2(){
+  
+      synth.triggerRelease(now+1)
+       document.getElementById("button").style.backgroundColor = "white";
+      };
+   
+
+  
 
 document.getElementById("button1").addEventListener("click", buttonDrums);
 
 function buttonDrums(){
   Tone.loaded().then(() => {
       drums.start();
+      document.getElementById("button1").style.backgroundColor = "lightpink";
   });
 } 
+
