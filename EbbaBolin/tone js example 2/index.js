ba12f08f9980d8bf52bc833465bfbdbd46ff2e16
ebbabolin <78794264+ebbabolin@users.noreply.github.com>
2021-02-26 10:42:57 +0100

    drums

--- EbbaBolin/tone js example 2/index.js
+++ EbbaBolin/tone js example 2/index.js
@@ -3,11 +3,21 @@ const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
 const guitar = new Tone.Player("./sounds/guitar.wav").toDestination();
+const drums = new Tone.Player("./sounds/drums.wav").toDestination();
 
-document.getElementById("button").addEventListener("click", mouseclick);
+document.getElementById("button").addEventListener("click", buttonGuitar);
 
-  function mouseclick(){
+  function buttonGuitar(){
     Tone.loaded().then(() => {
         guitar.start();
     });
   } 
+
+
+document.getElementById("button1").addEventListener("click", buttonDrums);
+
+function buttonDrums(){
+  Tone.loaded().then(() => {
+      drums.start();
+  });
+} 
