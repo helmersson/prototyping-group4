2fbc16c22df7bd4abb2f9cc14ef0311bbb33e1e9
RenatoRibe <78792187+RenatoRibe@users.noreply.github.com>
2021-03-02 11:03:38 +0100

    update Loop and add stop function

--- RenatoRibeiro/Siren_Test1.js/index.js
+++ RenatoRibeiro/Siren_Test1.js/index.js
@@ -1,37 +1,41 @@
 
 const button = document.getElementById("button");
+const buttonStop = document.getElementById("buttonStop");
 
-//create a synth and connect it to the master output (your speakers)
+
+
+//create a synth and connect it to the master output
 const synth = new Tone.MonoSynth({
+
     oscillator: {
         type: "square"
     },
     envelope: {
-        attack: 0.1
+        attack: 0.1,
+
     }
 }).toDestination();
 
 
 function siren() {
+    //starting notes of the siren
+    noteLow = 392;
+    noteHigh = 523;
 
-    //now = Tone.now();
-    const loop = new Tone.Loop((time) => {
+    now = Tone.now();
+    const loop = new Tone.Loop((now) => {
 
-        synth.triggerAttackRelease("C5", "4n", time);
-        synth.triggerAttackRelease("G4", "4n", time + 0.5);
-        /*synth.triggerAttackRelease("C5", "8n", now + 1.50);
-        synth.triggerAttackRelease("F#4", "8n", now + 2.25);
-        synth.triggerAttackRelease("B4", "8n", now + 3);
-        synth.triggerAttackRelease("F#4", "8n", now + 3.75);
-        synth.triggerAttackRelease("A#4", "8n", now + 4.50);
-        synth.triggerAttackRelease("F4", "8n", now + 5.25);*/
-        console.log(time);
-    }, "2n").start(0);
+        synth.triggerAttackRelease(noteHigh, "4n", now);
+        synth.triggerAttackRelease(noteLow, "4n", now + 0.5);
+
+        //Lower pitch on every iteration
+        noteLow = noteLow - 10;
+        noteHigh = noteHigh - 10;
+
+
+    }, "2n", 1, 2).start(now);
+
     Tone.Transport.start();
-
-
-    //const time = Tone.Time("8n").toSeconds();
-    //console.log(time);
 
 }
 
@@ -40,6 +44,12 @@ function siren() {
 button.addEventListener("click", () => {
     Tone.start();
     siren();
+
 })
 
+buttonStop.addEventListener("click", () => {
+    Tone.Transport.stop();
+    Tone.Transport.clear();
+
+})
 
