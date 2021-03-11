58a3a6ecaa2a12dbbe49669ac4acf2d571e4d2e6
Helmersson <50464822+helmersson@users.noreply.github.com>
2021-03-11 10:34:24 +0100

    updated the first example with more drums

--- WilhelmHelmersson/tone.js test/app.js
+++ WilhelmHelmersson/tone.js test/app.js
@@ -1,13 +1,19 @@
 function sequencer() {
-    const hihat = new Tone.Player("./drums/hihat-808.wav").toMaster();
-    const snare = new Tone.Player("./drums/snare-808.wav").toMaster();
+    //Imports all the drums that I'm using
+    const hihat = new Tone.Player("./drums/hihat-808.wav").toDestination();
+    const snare = new Tone.Player("./drums/snare-808.wav").toDestination();
+    const openhat = new Tone.Player("./drums/openhat-808.wav").toDestination();
+    const perc = new Tone.Player("./drums/perc-808.wav").toDestination();
+
+    //sets index to zero to ease the loop later on
     let index = 0;
 
-    //const hihatInputs = document.querySelectorAll('.hihat');
-    //const snareInputs = document.querySelectorAll('.snare');
+    //cycles the loop for 8 notes and then repeats it
     Tone.Transport.scheduleRepeat(repeat,"8n")
+    //transport must be called before anything happens
     Tone.Transport.start();
 
+    //A loop for the drum machine
     function repeat(){
         let step = index % 8;
         let hihatInputs = document.querySelector(
@@ -16,11 +22,23 @@ function sequencer() {
         let snareInputs = document.querySelector(
             `.snare input:nth-child(${step+1})`
         );
+        let openhatInputs = document.querySelector(
+            `.openhat input:nth-child(${step+1})`
+        );
+        let percInputs = document.querySelector(
+            `.perc input:nth-child(${step+1})`
+        );
         if(hihatInputs.checked){
             hihat.start();
         }
         if(snareInputs.checked){
             snare.start();
+        }
+        if(openhatInputs.checked){
+            openhat.start();
+        }
+        if(percInputs.checked){
+            perc.start();
         }
         index++;
     }
--- WilhelmHelmersson/tone_test_2/app.js
+++ WilhelmHelmersson/tone_test_2/app.js
@@ -1,40 +1,222 @@
-const midi = new Midi()
-
-// fetches the button from the HTML
-const button = document.getElementById('button')
-
-
-// Tone.js
-var synth = new Tone.PolySynth( Tone.Synth, {
+const synth = new Tone.PolySynth(Tone.Synth, {
     oscillator: {
-        type: "sine3"
+        type: "fatsawtooth",
+        count: 3,
+        spread: 30
     },
     envelope: {
-        attack: .03,
-        decay: .1,
-        sustain: .2,
-        release: .6
-    }
-}).toDestination()
-
-function playNote(time, event) {
-    synth.triggerAttackRelease(event.name, event.duration, time, event.velocity)
+        attack: 0.01,
+        decay: 0.1,
+        sustain: 0.5,
+        release: 0.4,
+        attackCurve: "exponential"
+    },
+}).toDestination();
+        
+// Van Halen - Jump MIDI from http://www.midiworld.com/files/1121/
+// converted using 
+const part = new Tone.Part(((time, note) => {
+    synth.triggerAttackRelease(note.noteName, note.duration, time, note.velocity);
+}), [{
+    time: "192i",
+    noteName: "G4",
+    velocity: 0.8110236220472441,
+    duration: "104i"
+},
+{
+    time: "192i",
+    noteName: "B4",
+    velocity: 0.7874015748031497,
+    duration: "104i"
+},
+{
+    time: "192i",
+    noteName: "D5",
+    velocity: 0.8031496062992126,
+    duration: "104i"
+},
+{
+    time: "480i",
+    noteName: "G4",
+    velocity: 0.7559055118110236,
+    duration: "104i"
+},
+{
+    time: "480i",
+    noteName: "C5",
+    velocity: 0.6850393700787402,
+    duration: "104i"
+},
+{
+    time: "480i",
+    noteName: "E5",
+    velocity: 0.6771653543307087,
+    duration: "104i"
+},
+{
+    time: "768i",
+    noteName: "F4",
+    velocity: 0.8661417322834646,
+    duration: "104i"
+},
+{
+    time: "768i",
+    noteName: "A4",
+    velocity: 0.8346456692913385,
+    duration: "104i"
+},
+{
+    time: "768i",
+    noteName: "C5",
+    velocity: 0.8188976377952756,
+    duration: "104i"
+},
+{
+    time: "1056i",
+    noteName: "F4",
+    velocity: 0.7007874015748031,
+    duration: "104i"
+},
+{
+    time: "1056i",
+    noteName: "A4",
+    velocity: 0.6850393700787402,
+    duration: "104i"
+},
+{
+    time: "1056i",
+    noteName: "C5",
+    velocity: 0.6614173228346457,
+    duration: "104i"
+},
+{
+    time: "1248i",
+    noteName: "G4",
+    velocity: 0.6771653543307087,
+    duration: "104i"
+},
+{
+    time: "1248i",
+    noteName: "B4",
+    velocity: 0.6771653543307087,
+    duration: "104i"
+},
+{
+    time: "1248i",
+    noteName: "D5",
+    velocity: 0.7165354330708661,
+    duration: "104i"
+},
+{
+    time: "1440i",
+    noteName: "G4",
+    velocity: 0.8818897637795275,
+    duration: "248i"
+},
+{
+    time: "1440i",
+    noteName: "B4",
+    velocity: 0.84251968503937,
+    duration: "248i"
+},
+{
+    time: "1440i",
+    noteName: "D5",
+    velocity: 0.8818897637795275,
+    duration: "248i"
+},
+{
+    time: "1728i",
+    noteName: "G4",
+    velocity: 0.8267716535433071,
+    duration: "104i"
+},
+{
+    time: "1728i",
+    noteName: "C5",
+    velocity: 0.8031496062992126,
+    duration: "104i"
+},
+{
+    time: "1728i",
+    noteName: "E5",
+    velocity: 0.8188976377952756,
+    duration: "104i"
+},
+{
+    time: "2016i",
+    noteName: "F4",
+    velocity: 0.7086614173228346,
+    duration: "104i"
+},
+{
+    time: "2016i",
+    noteName: "A4",
+    velocity: 0.7244094488188977,
+    duration: "104i"
+},
+{
+    time: "2016i",
+    noteName: "C5",
+    velocity: 0.7007874015748031,
+    duration: "104i"
+},
+{
+    time: "2208i",
+    noteName: "C4",
+    velocity: 0.9921259842519685,
+    duration: "296i"
+},
+{
+    time: "2208i",
+    noteName: "F4",
+    velocity: 0.968503937007874,
+    duration: "200i"
+},
+{
+    time: "2208i",
+    noteName: "A4",
+    velocity: 0.9606299212598425,
+    duration: "208i"
+},
+{
+    time: "2400i",
+    noteName: "E4",
+    velocity: 0.7559055118110236,
+    duration: "104i"
+},
+{
+    time: "2400i",
+    noteName: "G4",
+    velocity: 0.7007874015748031,
+    duration: "104i"
+},
+{
+    time: "2592i",
+    noteName: "C4",
+    velocity: 0.968503937007874,
+    duration: "488i"
+},
+{
+    time: "2592i",
+    noteName: "D4",
+    velocity: 0.9448818897637795,
+    duration: "488i"
+},
+{
+    time: "2592i",
+    noteName: "G4",
+    velocity: 0.937007874015748,
+    duration: "488i"
 }
+]).start(0);
+
+part.loop = true;
+part.loopEnd = "4m";
+
+Tone.Transport.bpm.value = 132;
 
 
-document.querySelector("button").addEventListener(".click", ()=> {
-    Tone.start();
-    // load a midi file in the browser
-async function playMidi() {
-    
-    const midi = await new Midi.fromUrl("./midi/dr-dre.mid") 
-    return midi;
-}
-const song = playMidi();
 
-console.log(song);
-Tone.Transport.bpm = song.bpm
-Tone.Transport.timeSignature = song.timeSignature
-Tone.Transport.start("0")
-new Tone.Part(playNote, song.track.notes).start(0)
-});
+// bind the interface
+document.querySelector("button").addEventListener("start", () => Tone.Transport.start());
